8e02be98fefa4d99c7e25276648d7ff59a68515e
MigashaneVictoire <Migashanevictoire@gmail.com>
2023-06-09 11:49:31 -0500

    re-evaluate prepare

--- prepare.py
+++ prepare.py
@@ -1,3 +1,9 @@
+# For funtion annotations
+from typing import Union
+from typing import Tuple
+
+import pandas as pd
+import numpy as np
 import acquire
 from sklearn.model_selection import train_test_split
 
@@ -33,60 +39,108 @@ def get_prep_insight(df):
 # Create a function named prep_iris that accepts the untransformed iris data, and returns the data with the transformations above applied.
 
 def prep_iris():
-    '''
-    This function will clean the the iris dataset
-    '''
     iris = acquire.get_iris_data()
     
     # Drop the species_id and measurement_id columns.
-    iris = iris.drop(columns="species_id")
+    iris = iris.drop(columns=["species_id"])
     
     # Rename the species_name column to just species.
     iris = iris.rename(columns={"species_name":"species"})
     
-    # Create dummy variables of the species name and concatenate onto the iris dataframe.
-    return pd.concat([iris,pd.get_dummies(iris.species)], axis=1)
+    # get dummy variables
+    get_dummies = pd.get_dummies(iris.species, drop_first=True)
+    
+    # Concatenate dummies to the iris dataframe.
+    iris = pd.concat([iris,get_dummies], axis=1)
+    return iris
+    
 
-def prep_titanic(df):
+def prep_titanic():
     '''
     This function will clean the the titanic dataset
     '''
-    df = df.drop(columns =['embark_town','class','age','deck'])
-
-    df.embarked = df.embarked.fillna(value='S')
+    # Data
+    titanic = acquire.get_titanic_data()
     
-    # Create a function named prep_telco that accepts the raw telco data, and returns the data with the transformations above applied.
-    dummy_df = pd.get_dummies(df[['sex','embarked']], drop_first=True)
-    df = pd.concat([df, dummy_df], axis=1)
-    return df
+    # drop the class column from the dataframe becuase it is a cuplicate column
+    # drop duplicates and un necessery columns
+    drop_cols = ["class", "deck", "embark_town"]
+    titanic = titanic.drop(columns=drop_cols, axis=1)
+    
+    # separeate numeric from object data types and get columns names
+    object_titanic = titanic.select_dtypes("object").columns
+
+    # make dummies for all categorical variables
+    all_dummies = pd.get_dummies(titanic[object_titanic], drop_first=True)
+    
+    # concate the dummies back to the data frame
+    titanic = pd.concat([titanic, all_dummies], axis=1)
+    
+    return titanic
 
 
-def prep_telco(telco):
-    # drop columns
+def prep_telco():
+    
+    telco = acquire.get_telco()
+    
+    # remove the non numerics in the total charges from data frame
+    print("original size:",len(telco))
+    telco = telco[telco.total_charges.str.contains(" ") == False]
+
+    # change the data type of total charges
+    telco.total_charges = telco.total_charges.astype("float")
+    telco.total_charges.dtype
+    
+    # drop duplicates and un necessery columns
     drop_cols = ["customer_id"]
     telco = telco.drop(columns=drop_cols, axis=1)
+
+    # separate numeric from object columns
+    object_telco = telco.select_dtypes("object").columns
     
-    # create dummy variables
-    dummy_cols = ["gender","phone_service", "tech_support", "streaming_tv","streaming_movies","paperless_billing","contract_type", "internet_service_type", "churn", "payment_type"]
-    all_dummies = pd.get_dummies(telco[dummy_cols], drop_first=True)
+    # get dummie variables
+    all_dummies = pd.get_dummies(telco[object_telco], drop_first=True)
     
-    telco = pd.concat([telco, all_dummies], axis=1)
+    # conect dummies to the originaal data
+    telco = pd.concat([telco,all_dummies], axis=1)
+    
     return telco
-    
 
 
-def train_val_test_split(df, target_col):
-    """
-    Takes in the titanic dataframe and return train, validate, test subset dataframes
-    """
-    #first split
-    train_val, test = train_test_split(df, #dataframe
-                    random_state= 95, #setting my random seed
-                    test_size= 0.20, #setting the size of my test df
-                     stratify=df[target_col]) #stratifying on my target variable
-    # second split
-    train,validate = train_test_split(train_val,
-                                     random_state=90,
-                                      test_size=0.25,
-                                      stratify=train_val[target_col])
+
+# Split the data into train, validate and train
+def split_data_(df: pd.DataFrame, test_size: float =.2, validate_size: float =.2, stratify_col: str =None, random_state: int =None) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    '''
+    parameters:
+        df: pandas dataframe you wish to split
+        test_size: size of your test dataset
+        validate_size: size of your validation dataset
+        stratify_col: the column to do the stratification on
+        random_state: random seed for the data
+
+    return:
+        train, validate, test DataFrames
+    '''
+    # no stratification
+    if stratify_col == None:
+        # split test data
+        train_validate, test = train_test_split(df, 
+                                                test_size=test_size, 
+                                                random_state=random_state)
+        # split validate data
+        train, validate = train_test_split(train_validate, 
+                                           test_size=validate_size/(1-test_size),
+                                            random_state=random_state)
+    # stratify split
+    else:
+        # split test data
+        train_validate, test = train_test_split(df,
+                                                test_size=test_size,
+                                                random_state=random_state, 
+                                                stratify=df[stratify_col])
+        # split validate data
+        train, validate = train_test_split(train_validate, 
+                                           test_size=validate_size/(1-test_size),
+                                           random_state=random_state, 
+                                           stratify=train_validate[stratify_col])
     return train, validate, test
